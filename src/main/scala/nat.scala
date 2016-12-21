70e8cfd45a7ae197d7d058f682145c53429d1861
dstechenko <dmytro.stechenko@gmail.com>
2016-12-20 21:04:08 -0500

    Refactor nat encoding

--- src/main/scala/nat.scala
+++ src/main/scala/nat.scala
@@ -5,20 +5,24 @@ import Bool._
 
 sealed trait Nat {
   type Pred <: Nat
+
   type Plus[N <: Nat] <: Nat
+  type Min[N <: Nat, O <: Nat, NO <: Nat] <: Nat
+
   type IsZero <: Bool
   type Equal[N <: Nat] <: Bool
   type LowerEqual[N <: Nat] <: Bool
-  type Min[N <: Nat, LO <: Nat, RO <: Nat] <: Nat
 }
 
-trait NatSyntax {
+trait NatFunctions {
+  type pred[N <: Nat] = N#Pred
+
   type +[L <: Nat, R <: Nat] = L#Plus[R]
+  type min[L <: Nat, R <: Nat] = L#Min[R, L, R]
+
+  type isZero[N <: Nat] = N#IsZero
   type ==[L <: Nat, R <: Nat] = L#Equal[R]
   type <=[L <: Nat, R <: Nat] = L#LowerEqual[R]
-  type isZero[N <: Nat] = N#IsZero
-  type pred[N <: Nat] = N#Pred
-  type min[L <: Nat, R <: Nat] = L#Min[R, L, R]
 }
 
 trait NatInstances {
@@ -34,22 +38,26 @@ trait NatInstances {
   type _9 = Succ[_8]
 }
 
-object Nat extends NatSyntax with NatInstances
+object Nat extends NatFunctions with NatInstances
 
 sealed trait Succ[P <: Nat] extends Nat {
   override type Pred = P
+
   override type Plus[N <: Nat] = Succ[Pred + N]
+  override type Min[N <: Nat, O <: Nat, NO <: Nat] = ifn[isZero[N], NO, Pred#Min[pred[N], O, NO]]
+
   override type IsZero = False
   override type Equal[N <: Nat] = ifb[isZero[N], False, Pred#Equal[pred[N]]]
   override type LowerEqual[N <: Nat] = ifb[isZero[N], False, Pred#LowerEqual[pred[N]]]
-  override type Min[N <: Nat, LO <: Nat, RO <: Nat] = ifn[isZero[N], RO, Pred#Min[pred[N], LO, RO]]
 }
 
 sealed trait Zero extends Nat {
   override type Pred = Zero
+
   override type Plus[N <: Nat] = N
+  override type Min[N <: Nat, O <: Nat, NO <: Nat] = O
+
   override type IsZero = True
   override type Equal[N <: Nat] = isZero[N]
   override type LowerEqual[N <: Nat] = True
-  override type Min[N <: Nat, LO <: Nat, RO <: Nat] = LO
 }
