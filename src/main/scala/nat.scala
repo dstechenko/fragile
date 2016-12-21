695e3eef2a52582b6221651c3dde05cb434eedb4
dstechenko <dmytro.stechenko@gmail.com>
2016-12-21 16:14:58 -0500

    Format nat module

--- src/main/scala/nat.scala
+++ src/main/scala/nat.scala
@@ -4,32 +4,29 @@ import Nat._
 import Bool._
 
 sealed trait Nat {
-  type Pred <: Nat
-  type Plus[N <: Nat] <: Nat
-  type Mult[N <: Nat] <: Nat
-  type Min[N <: Nat, O <: Nat, NO <: Nat] <: Nat
-  type Max[N <: Nat, O <: Nat, NO <: Nat] <: Nat
-  type IsZero <: Bool
-  type Equal[N <: Nat] <: Bool
-  type NotEqual[N <: Nat] = ![Equal[N]]
-  type LowerEqual[N <: Nat] <: Bool
-  type GreaterEqual[N <: Nat] = Greater[N] || Equal[N]
-  type Lower[N <: Nat]        = ![GreaterEqual[N]]
-  type Greater[N <: Nat]      = ![LowerEqual[N]]
+  type Pred                                  <: Nat
+  type Plus  [N <: Nat]                      <: Nat
+  type Mult  [N <: Nat]                      <: Nat
+  type Min   [N <: Nat, O <: Nat, NO <: Nat] <: Nat
+  type IsZero                                <: Bool
+  type Equal [N <: Nat]                      <: Bool
+  type Lower [N <: Nat]                      <: Bool
 }
 
 trait NatFunctions {
-  type pred[N <: Nat]          = N#Pred
-  type +[L <: Nat, R <: Nat]   = L#Plus[R]
-  type min[L <: Nat, R <: Nat] = L#Min[R, L, R]
-  type max[L <: Nat, R <: Nat] = L#Max[R, L, R]
-  type isZero[N <: Nat]        = N#IsZero
-  type ==[L <: Nat, R <: Nat]  = L#Equal[R]
-  type =/=[L <: Nat, R <: Nat] = L#NotEqual[R]
-  type <=[L <: Nat, R <: Nat]  = L#LowerEqual[R]
-  type >=[L <: Nat, R <: Nat]  = L#GreaterEqual[R]
-  type <[L <: Nat, R <: Nat]   = L#Lower[R]
-  type >[L <: Nat, R <: Nat]   = L#Greater[R]
+  type pred  [N <: Nat]           = N#Pred
+  type +     [L <: Nat, R <: Nat] = L#Plus[R]
+  type *     [L <: Nat, R <: Nat] = L#Mult[R]
+  type min   [L <: Nat, R <: Nat] = L#Min[R, L, R]
+  type isZero[N <: Nat]           = N#IsZero
+  type ==    [L <: Nat, R <: Nat] = L#Equal[R]
+  type <     [L <: Nat, R <: Nat] = L#Lower[R]
+
+  type max   [L <: Nat, R <: Nat] = ifn[L == min[L, R], R, L]
+  type =/=   [L <: Nat, R <: Nat] = ![L == R]
+  type >     [L <: Nat, R <: Nat] = ![L <= R]
+  type <=    [L <: Nat, R <: Nat] = ==[L, R] || <[L, R]
+  type >=    [L <: Nat, R <: Nat] = ==[L, R] || >[L, R]
 }
 
 trait NatInstances {
@@ -48,21 +45,22 @@ trait NatInstances {
 object Nat extends NatFunctions with NatInstances
 
 sealed trait Succ[P <: Nat] extends Nat {
-  override type Pred                               = P
-  override type Plus[N <: Nat]                     = Succ[Pred + N]
-  override type Min[N <: Nat, O <: Nat, NO <: Nat] = ifn[isZero[N], NO, Pred#Min[pred[N], O, NO]]
-  override type Max[N <: Nat, O <: Nat, NO <: Nat] = ifn[isZero[N], O, Pred#Max[pred[N], O, NO]]
-  override type IsZero                             = False
-  override type Equal[N <: Nat]                    = ifb[isZero[N], False, Pred#Equal[pred[N]]]
-  override type LowerEqual[N <: Nat]               = ifb[isZero[N], False, Pred#LowerEqual[pred[N]]]
+  override type Pred                                  = P
+  override type Plus  [N <: Nat]                      = Succ[Pred + N]
+  override type Min   [N <: Nat, O <: Nat, NO <: Nat] = ifn[isZero[N], NO, Pred#Min[pred[N], O, NO]]
+  override type IsZero                                = False
+
+  private type loop   [N <: Nat, F[_ <: Nat] <: Bool] = ifb[isZero[N], False, F[pred[N]]]
+
+  override type Equal [N <: Nat]                      = loop[N, Pred#Equal]
+  override type Lower [N <: Nat]                      = loop[N, Pred#Lower]
 }
 
 sealed trait Zero extends Nat {
-  override type Pred                               = Zero
-  override type Plus[N <: Nat]                     = N
-  override type Min[N <: Nat, O <: Nat, NO <: Nat] = O
-  override type Max[N <: Nat, O <: Nat, NO <: Nat] = NO
-  override type IsZero                             = True
-  override type Equal[N <: Nat]                    = isZero[N]
-  override type LowerEqual[N <: Nat]               = True
+  override type Pred                                  = Zero
+  override type Plus  [N <: Nat]                      = N
+  override type Min   [N <: Nat, O <: Nat, NO <: Nat] = O
+  override type IsZero                                = True
+  override type Equal [N <: Nat]                      = isZero[N]
+  override type Lower [N <: Nat]                      = ![Equal[N]]
 }
