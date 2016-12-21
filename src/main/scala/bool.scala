9336b298e5a2aa162313b5483837e2ad75796ed3
dstechenko <dmytro.stechenko@gmail.com>
2016-12-21 16:17:52 -0500

    Format bool module

--- src/main/scala/bool.scala
+++ src/main/scala/bool.scala
@@ -1,42 +1,42 @@
 import language.higherKinds
 
 sealed trait Bool {
-  type Not <: Bool
-  type Or[B <: Bool] <: Bool
-  type And[B <: Bool] <: Bool
+  type Not                       <: Bool
+  type Or [B <: Bool]            <: Bool
+  type And[B <: Bool]            <: Bool
 
   type IfB[T <: Bool, E <: Bool] <: Bool
-  type IfN[T <: Nat, E <: Nat] <: Nat
+  type IfN[T <: Nat,  E <: Nat]  <: Nat
   type IfL[T <: List, E <: List] <: List
 }
 
 sealed trait True extends Bool {
-  override type Not            = False
-  override type Or[B <: Bool]  = True
-  override type And[B <: Bool] = B
+  override type Not                       = False
+  override type Or [B <: Bool]            = True
+  override type And[B <: Bool]            = B
 
   override type IfB[T <: Bool, E <: Bool] = T
-  override type IfN[T <: Nat, E <: Nat]   = T
+  override type IfN[T <: Nat,  E <: Nat]  = T
   override type IfL[T <: List, E <: List] = T
 }
 
 sealed trait False extends Bool {
-  override type Not            = True
-  override type Or[B <: Bool]  = B
-  override type And[B <: Bool] = False
+  override type Not                       = True
+  override type Or [B <: Bool]            = B
+  override type And[B <: Bool]            = False
 
   override type IfB[T <: Bool, E <: Bool] = E
-  override type IfN[T <: Nat, E <: Nat]   = E
+  override type IfN[T <: Nat,  E <: Nat]  = E
   override type IfL[T <: List, E <: List] = E
 }
 
 trait BoolFunctions {
-  type &&[L <: Bool, R <: Bool] = L#And[R]
-  type ||[L <: Bool, R <: Bool] = L#Or[R]
-  type ![B <: Bool]             = B#Not
+  type && [L <: Bool, R <: Bool]            = L#And[R]
+  type || [L <: Bool, R <: Bool]            = L#Or[R]
+  type !  [B <: Bool]                       = B#Not
 
   type ifb[B <: Bool, T <: Bool, E <: Bool] = B#IfB[T, E]
-  type ifn[B <: Bool, T <: Nat, E <: Nat]   = B#IfN[T, E]
+  type ifn[B <: Bool, T <: Nat,  E <: Nat]  = B#IfN[T, E]
   type ifl[B <: Bool, T <: List, E <: List] = B#IfL[T, E]
 }
 
