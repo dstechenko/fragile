1be8083625b6623d181bfb6407b840683024afa1
dstechenko <dmytro.stechenko@gmail.com>
2016-12-21 21:05:42 -0500

    Add all missing api stubs

--- src/main/scala/list.scala
+++ src/main/scala/list.scala
@@ -52,38 +52,55 @@ sealed trait Nil extends List {
 }
 
 trait TListFunctions {
-  type map        [L <: List, F[_ <: Nat] <: Nat]                     = L#Map[F]
-  type filter     [L <: List, F[_ <: Nat] <: Bool]                    = L#Filter[F]
-  type fold       [L <: List, N <: Nat, F[_ <: Nat, _ <: Nat] <: Nat] = L#Fold[N, F]
-  type indexOf    [L <: List, N <: Nat]                               = L#IndexOf[N, _0]
-  type remove     [L <: List, N <: Nat]                               = L#Remove[N]
-  type :::        [L <: List, R <: List]                              = L#Concat[R]
-  type reverse    [L <: List]                                         = L#Reverse
-  type sort       [L <: List]                                         = L#Sort
-  type size       [L <: List]                                         = L#Size
+  type map             [L <: List, F[_ <: Nat] <: Nat]                     = L#Map[F]
+  type filter          [L <: List, F[_ <: Nat] <: Bool]                    = L#Filter[F]
+  type fold            [L <: List, N <: Nat, F[_ <: Nat, _ <: Nat] <: Nat] = L#Fold[N, F]
+  type indexOf         [L <: List, N <: Nat]                               = L#IndexOf[N, _0]
+  type remove          [L <: List, N <: Nat]                               = L#Remove[N]
+  type :::             [L <: List, R <: List]                              = L#Concat[R]
+  type reverse         [L <: List]                                         = L#Reverse
+  type sort            [L <: List]                                         = L#Sort
+  type size            [L <: List]                                         = L#Size
 
-  type isEmpty    [L <: List]                                         = size[L] == _0
-  type nonEmpty   [L <: List]                                         = ![isEmpty[L]]
-  type reduceP    [L <: List, F[_ <: Nat, _ <: Nat] <: Nat]           = fold[L, _0, F]
-  type reduceM    [L <: List, F[_ <: Nat, _ <: Nat] <: Nat]           = fold[L, _1, F]
-  type sum        [L <: List]                                         = L reduceP ({ type F[LN <: Nat, RN <: Nat] = RN + LN })#F
-  type product    [L <: List]                                         = L reduceM ({ type F[LN <: Nat, RN <: Nat] = RN * LN })#F
-  type count      [L <: List, F[_ <: Nat] <: Bool]                    = size[L filter F]
-  type contains   [L <: List, M <: Nat]                               = nonEmpty[L filter ({ type F[N <: Nat] = M == N })#F]
-  type exists     [L <: List, F[_ <: Nat] <: Bool]                    = (L count F) > _0
-  type filterNot  [L <: List, F[_ <: Nat] <: Bool]                    = L filter ({ type FN[N <: Nat] = ![F[N]] })#FN
-  type forall     [L <: List, F[_ <: Nat] <: Bool]                    = ![L exists ({ type FN[N <: Nat] = ![F[N]] })#FN]
-  type sortAsc    [L <: List]                                         = sort[L]
-  type sortDesc   [L <: List]                                         = reverse[sortAsc[L]]
-  type corresponds[L <: List, R <: List]                              = L forall ({ type F[N <: Nat] = (L indexOf N) == (R indexOf N) })#F
-  type ===        [L <: List, R <: List]                              = (size[L] == size[R]) && (L corresponds R) && (R corresponds L)
+  type isEmpty         [L <: List]                                         = size[L] == _0
+  type nonEmpty        [L <: List]                                         = ![isEmpty[L]]
+  type reduceP         [L <: List, F[_ <: Nat, _ <: Nat] <: Nat]           = fold[L, _0, F]
+  type reduceM         [L <: List, F[_ <: Nat, _ <: Nat] <: Nat]           = fold[L, _1, F]
+  type sum             [L <: List]                                         = L reduceP ({ type F[LN <: Nat, RN <: Nat] = RN + LN })#F
+  type product         [L <: List]                                         = L reduceM ({ type F[LN <: Nat, RN <: Nat] = RN * LN })#F
+  type count           [L <: List, F[_ <: Nat] <: Bool]                    = size[L filter F]
+  type contains        [L <: List, M <: Nat]                               = nonEmpty[L filter ({ type F[N <: Nat] = M == N })#F]
+  type exists          [L <: List, F[_ <: Nat] <: Bool]                    = (L count F) > _0
+  type filterNot       [L <: List, F[_ <: Nat] <: Bool]                    = L filter ({ type FN[N <: Nat] = ![F[N]] })#FN
+  type forall          [L <: List, F[_ <: Nat] <: Bool]                    = ![L exists ({ type FN[N <: Nat] = ![F[N]] })#FN]
+  type sortAsc         [L <: List]                                         = sort[L]
+  type sortDesc        [L <: List]                                         = reverse[sortAsc[L]]
+  type corresponds     [L <: List, R <: List]                              = L forall ({ type F[N <: Nat] = (L indexOf N) == (R indexOf N) })#F
+  type ===             [L <: List, R <: List]                              = (size[L] == size[R]) && (L corresponds R) && (R corresponds L)
+  type isDefinedAt     [L <: List, N <: Nat]                               = size[L] >= N
 
-  type distinct   [L <: List]                                         = Nothing
-  type dropLeft   [L <: List, N <: Nat]                               = Nothing
-  type dropRight  [L <: List, N <: Nat]                               = Nothing
-  type dropWhile  [L <: List, F[_ <: Nat] <: Bool]                    = Nothing
-  type take       [L <: List, N <: Nat]                               = Nothing
-  type takeWhile  [L <: List, F[_ <: Nat] <: Bool]                    = Nothing
+  type indexWhere      [L <: List, F[_ <: Nat] <: Bool]                    = Nothing
+  type indexWhereFrom  [L <: List, F[_ <: Nat] <: Bool, B <: Nat]          = Nothing
+  type indexOfSlice    [L <: List, R <: List]                              = Nothing
+  type indexOfSliceFrom[L <: List, R <: List, B <: Nat]                    = Nothing
+  type containsSlice   [L <: List, R <: List]                              = Nothing
+  type containsAll     [L <: List, R <: List]                              = Nothing
+  type distinct        [L <: List]                                         = Nothing
+  type dropLeft        [L <: List, N <: Nat]                               = Nothing
+  type dropRight       [L <: List, N <: Nat]                               = Nothing
+  type dropWhile       [L <: List, F[_ <: Nat] <: Bool]                    = Nothing
+  type takeLeft        [L <: List, N <: Nat]                               = Nothing
+  type takeRigh        [L <: List, N <: Nat]                               = Nothing
+  type takeWhile       [L <: List, F[_ <: Nat] <: Bool]                    = Nothing
+  type startsWith      [L <: List, R <: List]                              = Nothing
+  type startsWithOffset[L <: List, R <: List, O <: Nat]                    = Nothing
+  type endsWith        [L <: List, R <: List]                              = Nothing
+  type diff            [L <: List, R <: List]                              = Nothing
+  type union           [L <: List, R <: List]                              = Nothing
+  type intersect       [L <: List, R <: List]                              = Nothing
+  type permutations    [L <: List]                                         = Nothing
+  type slice           [L <: List, B <: Nat, E <: Nat]                     = Nothing
+  type flatMap         [L <: List, F[_ <: Nat] <: List]                    = Nothing
 }
 
 object List extends TListFunctions
