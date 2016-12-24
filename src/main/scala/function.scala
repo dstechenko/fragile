a90fac6d2c2b02d9faf81b5dffa21d5bcfc509c2
dstechenko <dmytro.stechenko@gmail.com>
2016-12-23 20:00:45 -0500

    Add function compose

--- src/main/scala/function.scala
+++ src/main/scala/function.scala
@@ -7,6 +7,7 @@ trait FunctionFunctions {
   type const   [C <: Nat]                                = Function { type Apply[N <: Nat] = C              }
   type eta     [L[_ <: Nat] <: Nat]                      = Function { type Apply[N <: Nat] = L[N]           }
   type andThen [L <: Function, R[_ <: Nat] <: Nat]       = Function { type Apply[N <: Nat] = R[apply[L, N]] }
+  type compose [L <: Function, R[_ <: Nat] <: Nat]       = Function { type Apply[N <: Nat] = apply[L, R[N]] }
   type apply   [F <: Function, N <: Nat]                 = F#Apply[N]
 }
 
--- src/main/scala/laws.scala
+++ src/main/scala/laws.scala
@@ -375,9 +375,10 @@ object IntersectLaws {
 
 object FunctionLaws {
   type increase[N <: Nat] = N + _1
-  type twice   [N <: Nat] = N * _2
+  type multiply[N <: Nat] = N * _2
 
-  implicitly[list[_1] map identity                                =:= list[_1]]
-  implicitly[const[_1] apply _2                                   =:= _1]
-  implicitly[(eta[increase] andThen twice andThen twice) apply _1 =:=       _8]
+  implicitly[list[_1] map identity                                      =:= list[_1]]
+  implicitly[const[_1] apply _2                                         =:=       _1]
+  implicitly[(eta[increase] andThen multiply andThen multiply) apply _1 =:=       _8]
+  implicitly[(eta[increase] compose multiply compose multiply) apply _1 =:=       _5]
 }
