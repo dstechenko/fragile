ba0436233488bb891645a50ed38bd21a77033c49
dstechenko <dmytro.stechenko@gmail.com>
2016-12-24 15:00:28 -0500

    Add int ++/--/**

--- src/main/scala/fragile/int/package.scala
+++ src/main/scala/fragile/int/package.scala
@@ -7,10 +7,34 @@ import fragile.pair._
 package object int {
   type Int                     = Pair[Nat]
   type int[N <: Nat]           = N <-> _0
+  type nat[I <: Int]           = _0
 
-  type ~  [I <: Int]           = secondN[I] <-> firstN[I]
-  type ++ [L <: Int, R <: Int] = (firstN[L] + firstN[R]) <-> (secondN[L] + secondN[R])
-  type -- [L <: Int, R <: Int] = L ++ ~[R]
-  type ** [L <: Int, R <: Int] = ((firstN[L] * firstN[R]) + (secondN[L] * secondN[R])) <-> ((firstN[L] * secondN[R]) + (secondN[L] * firstN[R]))
+  type canonical[I <: Int]     = ({
+                                   type isPositive = firstN[I] > secondN[I]
+                                   type first      = ifN[isPositive, firstN[I] - secondN[I], _0]
+                                   type second     = ifN[isPositive, _0, secondN[I] - firstN[I]]
+                                   type run        = first <-> second
+                                 })#run
+
   type eq [L <: Int, R <: Int] = False
+
+  type ~  [I <: Int]           = canonical[secondN[I] <-> firstN[I]]
+  type -- [L <: Int, R <: Int] = L ++ ~[R]
+
+  type ++ [L <: Int, R <: Int] = ({
+                                   type first  = firstN[L] + firstN[R]
+                                   type second = secondN[L] + secondN[R]
+                                   type run    = canonical[first <-> second]
+                                 })#run
+
+  type ** [L <: Int, R <: Int] = ({
+                                   type firstLeft   = firstN[L] * firstN[R]
+                                   type firstRight  = secondN[L] * secondN[R]
+                                   type first       = firstLeft + firstRight
+                                   type secondLeft  = firstN[L] * secondN[R]
+                                   type secondRight = secondN[L] * firstN[R]
+                                   type second      = secondLeft + secondRight
+                                   type run         = canonical[first <-> second]
+                                 })#run
+
 }
--- src/test/scala/fragile/int/laws.scala
+++ src/test/scala/fragile/int/laws.scala
@@ -4,15 +4,23 @@ import fragile.nat._
 
 object PlusLaws {
   implicitly[int[_2] ++ int[_3]                =:=                           int[_5]]
-  implicitly[(int[_2] ++ int[_3])              =:=              (int[_3] ++ int[_2])]
   implicitly[int[_3] ++ int[_0]                =:=                           int[_3]]
   implicitly[int[_0] ++ int[_3]                =:=                           int[_3]]
+  implicitly[(int[_2] ++ int[_3])              =:=              (int[_3] ++ int[_2])]
   implicitly[((int[_1] ++ int[_2]) ++ int[_3]) =:= (int[_1] ++ (int[_2] ++ int[_3]))]
 }
 
 object MinusLaws {
   implicitly[int[_0] -- int[_0] =:= int[_0]]
-  // implicitly[int[_3] -- int[_3] =:= int[_0]]
-  // implicitly[int[_3] -- int[_2] =:= int[_1]]
-  // implicitly[int[_2] -- int[_3] =:= (int[_0] -- int[_1])]
+  implicitly[int[_3] -- int[_3] =:= int[_0]]
+  implicitly[int[_3] -- int[_2] =:= int[_1]]
+  implicitly[int[_2] -- int[_3] =:= (int[_0] -- int[_1])]
+}
+
+object MultLaws {
+  implicitly[int[_2] ** int[_3]                =:=                           int[_6]]
+  implicitly[int[_3] ** int[_0]                =:=                           int[_0]]
+  implicitly[int[_0] ** int[_3]                =:=                           int[_0]]
+  implicitly[(int[_2] ** int[_3])              =:=              (int[_3] ** int[_2])]
+  implicitly[((int[_2] ** int[_3]) ** int[_4]) =:= (int[_2] ** (int[_3] ** int[_4]))]
 }
